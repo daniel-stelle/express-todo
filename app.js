9129e86cfdf5d3b129c645b62348181efa66cf1b
Daniel Stelle <daniel.stelle@codingzeal.com>
2017-03-18 19:47:47 -0700

    Add post route for creating todos

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ let db = pgp({
   port: 5432,
   database: 'express_todos'
 })
+let bodyParser = require('body-parser')
+let parseUrlencoded = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'))
 
 app.set('view engine', 'pug')
 
-app.get('/', function(request, response) {
+app.get('/todos', function(request, response) {
   db.any('select * from todos')
     .then(data => {
       response.render('index', { title: 'Hey', todos: data })
     })
+})
+
+app.post('/todos', parseUrlencoded, function(request, response) {
+  let newBlock = request.body
+  db.none('insert into todos(title, complete) values($1, $2)', [newBlock.title, false])
+  response.status(201).json(newBlock.title)
 })
 
 app.listen(3000, function() {
